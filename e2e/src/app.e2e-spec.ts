988801ad076f30f4cf92a11d0c99fc605073c7bc
richayua <richard.yuan@sogeti.com>
2019-06-28 13:23:15 +0200

    e2e done

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -4,19 +4,40 @@ import { DashboardPageTest } from './DashboardPageTest';
 import { protractor } from 'protractor/built/ptor';
 import { RegisterPageTest } from './RegisterPageTest';
 
-describe('new App', () => {
+describe('Account:', () => {
   const loginPage: LoginPageTest = new LoginPageTest();
   const dashboardPage: DashboardPageTest = new DashboardPageTest();
+  const registerPage: RegisterPageTest = new RegisterPageTest();
 
-  beforeEach(() => {
-    loginPage.load();
+  // beforeEach(() => {
+  //   loginPage.load();
+  // });
+
+  it('should register', () => {
+    registerPage.load();
+    registerPage.waitUntilVisible();
+    registerPage.enterEmail('test@test.com');
+    registerPage.enterPassword('password123');
+    registerPage.clickOnButton('.submit-btn');
+    registerPage.clickOnButton('.loginpage');
   });
 
   it('should login, retrieve data and visualise it from Firebase', () => {
-    loginPage.enterEmail('a@live.com');
+    loginPage.load();
+    loginPage.enterEmail('test@test.com');
     loginPage.enterPassword('password123');
     loginPage.clickOnButton('.submit-btn');
     dashboardPage.waitUntilVisible();
+    browser.sleep(1000);
+    dashboardPage.clickOnButton('.retrieveSwimtimes');
+    browser.sleep(1000);
+    dashboardPage.clickOnButton('.visualiseSwimtimes');
+    browser.sleep(1000);
+  });
+
+  it('should retrieve and visualise data', () => {
+    dashboardPage.waitUntilVisible();
+    browser.sleep(1000);
     dashboardPage.clickOnButton('.retrieveSwimtimes');
     browser.sleep(1000);
     dashboardPage.clickOnButton('.visualiseSwimtimes');
@@ -25,25 +46,35 @@ describe('new App', () => {
 
 });
 
-// describe('create new Acc', () => {
+// describe('Add data', () => {
 //   const loginPage: LoginPageTest = new LoginPageTest();
 //   const dashboardPage: DashboardPageTest = new DashboardPageTest();
-//   const registerPage: RegisterPageTest = new RegisterPageTest();
 
 //   beforeEach(() => {
 //     loginPage.load();
 //   });
 
-//   it('should register', () => {
-//     loginPage.clickOnButton('.register');
-//     registerPage.waitUntilVisible();
-//     registerPage.enterEmail('test@test.com');
-//     //registerPage.enterPassword('password123');
-//     registerPage.clickOnButton('.submit-btn');
+//   it('should add data to Cloud Firestore and visualise', () => {
+//     loginPage.enterEmail('test@test.com');
+//     loginPage.enterPassword('password123');
+//     loginPage.clickOnButton('.submit-btn');
+//     dashboardPage.waitUntilVisible();
+//     dashboardPage.clickOnButton('.sync');
+//     dashboardPage.clickOnButton('.retrieveSwimtimes');
+//     browser.sleep(500);
+//     dashboardPage.clickOnButton('.visualiseSwimtimes');
 //     browser.sleep(1000);
-//     // dashboardPage.clickOnButton('.retrieveSwimtimes');
-//     // browser.sleep(1000);
-//     // dashboardPage.clickOnButton('.visualiseSwimtimes');
-//     // browser.sleep(1000);
 //   });
 
+// //   it('should login', () => {
+// //     loginPage.load();
+// //     loginPage.waitUntilVisible();
+// //     loginPage.enterEmail('test@test.com');
+// //     loginPage.enterPassword('password123');
+// //     loginPage.clickOnButton('.submit-btn');
+// //     dashboardPage.waitUntilVisible();
+// //     dashboardPage.clickOnButton('.retrieveSwimlanes');
+// //     dashboardPage.clickOnButton('.visualiseSwimlanes');
+// //   });
+
+// })
